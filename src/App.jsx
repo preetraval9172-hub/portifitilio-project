5467b84adc11314070a62b40a3a94b112df52909
preetraval9172-hub <preetraval9172@gmail.com>
2026-06-22 11:24:03 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import portifilio from './Portifilio project/Portifilio'
+import Portifilio from './Portifilio project/Portifilio'
+
+const App = () => {
+  return (
+    <div>
+     <Portifilio/> 
+    </div>
+  )
+}
+
+export default App
